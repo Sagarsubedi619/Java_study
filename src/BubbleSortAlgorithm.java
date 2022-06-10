cac736a72b621f21843f1fed4fa2efa3121fea72
Sagar Subedi <sagarsubedi619@gmail.com>
2022-06-10 14:32:29 -0500

    selection sort implemented

--- src/BubbleSortAlgorithm.java
+++ src/BubbleSortAlgorithm.java
@@ -1,7 +1,7 @@
-public class Main {
-    public int[] BubbleSortAlgorithm(int[] arrayNumbers) {
+public class BubbleSortAlgorithm {
+    public int[] BubbleSortAlgorithmImplementation(int[] arrayNumbers) {
         int temp;
-        for (int i = 0; i < arrayNumbers.length - 1; i++) {
+        for (int i = 0; i < arrayNumbers.length - 2; i++) {
             for (int j = i + 1; j < arrayNumbers.length - 1; j++) {
                 if (arrayNumbers[i] > arrayNumbers[j]) {
                     temp = arrayNumbers[i];
@@ -12,12 +12,27 @@ public class Main {
         }
         return arrayNumbers;
     }
+
+    public int[] BubbleSortForDescending(int[] nums) {
+        int temp;
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                if (nums[i] < nums[j]) {
+                    temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+        return nums;
+    }
     public static void main(String[] args) {
-        Main demo1 = new Main();
-        int[] inputArray = {2, 1, 6, 5, 4, 3, 7, 0};
-        for (int i = 0; i < inputArray.length - 1; i++) {
-            System.out.println(demo1.BubbleSortAlgorithm(inputArray)[i]);
+        BubbleSortAlgorithm demo1 = new BubbleSortAlgorithm();
+        int[] inputArray = {2, 1, 6, 5, 4, 3, 7};
+        var x = demo1.BubbleSortAlgorithmImplementation(inputArray);
+        for (int i = 0; i < inputArray.length; i++) {
+            System.out.println(x[i]);
+            System.out.println(demo1.BubbleSortForDescending(inputArray)[i]);
         }
     }
-
 }
--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class SelectionSort {
+public class SelectionSort {
+
+    public int[] SelctionSortImplementation(int[] nums) {
+        int temp = 0;
+        for (int i = 0; i < nums.length - 2; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                if (nums[j] < nums[minIndex]) {
+                    minIndex = j;
+                    temp = nums[minIndex];
+                    nums[minIndex] = nums[i];
+                    nums[i] = temp;
+                }
+            }
+        }
+        return nums;
+    }
+
+    public int[] SelectionSortDescending(int[] nums){
+        int temp=0;
+        for(int i=0;i<nums.length-1;i++){
+            int maxIndex=i;
+            for (int j=i+1;j<nums.length-1;j++){
+                if(nums[maxIndex]<nums[j]){
+                   maxIndex=j;
+                   temp=nums[i];
+                   nums[i]=nums[maxIndex];
+                   nums[maxIndex]=temp;
+                }
+            }
+        }
+
+        return nums;
+    }
+
+
+    public static void main(String[] args) {
+        SelectionSort demo1 = new SelectionSort();
+        int[] inputArray = {2, 1, 6, 5, 4, 3, 7};
+        var x = demo1.SelctionSortImplementation(inputArray);
+        var y = demo1.SelectionSortDescending(inputArray);
+        for (int i = 0; i < inputArray.length-1; i++) {
+            System.out.println(y[i]);
+
+        }
+    }
 }
